287c6da074bfd3a6767bc3dbbad2fc0867d42e92
Rafay <rafay@Rafays-MacBook-Air.local>
2025-04-21 17:24:14 +0500

    sigin api updated

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -11,7 +11,7 @@ function SignIn() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:8000/login', {
+      const response = await fetch('https://pdf-auto-bd.vercel.app/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
